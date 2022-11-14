3dc962c59948e5a5795f81d707e6b682ddc6784e
Jonathan Ribeiro <jonathan.ribeiro61@gmail.com>
2022-11-14 15:34:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [Shortly URL shortening API Challenge challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview]
+  - [The challenge]
+  - [Screenshot]
+  - [Links]
+- [My process]
+  - [Built with]
+- [Author](#author)
+
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+Perfect challege to improve your front-end skills. 
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- Shorten any valid URL
+- See a list of their shortened links, even after refreshing the browser
+- Copy the shortened link to their clipboard in a single click
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+
+### Screenshot
+
+![](./images/screenshoot.jpg)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+I started structuring the challenge through HTML, 
+then I started the design with css (FLEXBOX), soon after I worked with javascript.
+In javascript I used events and functions and api request.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Javascript
+- Mobile-first workflow
+- Api request
+
+
+## Author
+
+- Website - [url shortening api request]()
+- Frontend Mentor - [@jowribas](https://www.frontendmentor.io/profile/jowribas)
+
---
+++ script.js
@@ -0,0 +1,54 @@
+const input = document.querySelector('.warning--link #input');
+const shortenBtn = document.querySelector('.warning--link #submit');
+const linkFull = document.querySelector('.link--full');
+const linkShorten = document.querySelector('.link--shorten');
+const model = document.querySelector('.historic--model');
+const warning = document.querySelector('.warning--addlink em');
+const historicArea = document.querySelector('.historic--area');
+
+let link = '';
+let modelHistoric = '';
+
+shortenBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    
+    link = input.value;
+
+    if(checkStatus()){
+        input.value = '';
+        modelHistoric = model.cloneNode(true);
+        
+        let results = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
+        let json = await results.json();
+
+        modelHistoric.querySelector('.link--full').innerHTML = link;
+        modelHistoric.querySelector('.link--shorten').innerHTML = json.result.short_link;
+  
+        modelHistoric.querySelectorAll('.copy').forEach((btn) => {
+            let key = modelHistoric.querySelector('.link--shorten').innerHTML;
+        
+            btn.addEventListener('click', (e) => {
+                navigator.clipboard.writeText(key).then(() => {
+                    e.target.style.backgroundColor = 'hsl(257, 27%, 26%)';
+                    e.target.innerHTML = 'Copied!'; 
+                }) 
+            })
+        }) 
+        historicArea.prepend(modelHistoric);
+    };
+});
+
+function checkStatus() {
+    if(link != ''){
+        warning.style.color = 'transparent';
+        input.classList.remove('warning--border');
+        input.classList.remove('warning--placeholder');;
+          return true
+    } else {
+        warning.style.color = '#f85e66';
+        input.classList.add('warning--border');
+        input.classList.add('warning--placeholder');
+          return false
+    }; 
+};
+    
